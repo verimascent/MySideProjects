580fcfdf37a502d9c13c5e83f79eb3b805d7eb4c
verimascent <verimascent@gmail.com>
2021-06-09 20:03:15 +0800

    minesweeper clone

---
+++ script.js
@@ -0,0 +1,76 @@
+import { 
+    TILE_STATUSES, 
+    createBoard, 
+    markTile, 
+    revealTile,
+    checkWin,
+    checkLose, 
+} from "./minesweeper.js";
+
+// Display / UI
+
+// 1. Populate a board with tiles/mines
+// 2. Left click on tiles
+    // a. Reveal tiles
+// 3. Right click on tiles
+    // a. Mark tiles
+// 4. Check for win/lose
+
+const BOARD_SIZE = 10;
+const NUMBER_OF_MINES = 3;
+
+const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
+const boardElement = document.querySelector(".board");
+const minesLeftText = document.querySelector("[data-mine-count]");
+const messageText = document.querySelector(".subtext");
+
+board.forEach(row => {
+    row.forEach(tile => {
+        boardElement.append(tile.element)
+        tile.element.addEventListener("click", () => {
+            revealTile(board, tile);
+            checkGameEnd();
+        });
+        tile.element.addEventListener("contextmenu", e => {
+            e.preventDefault();
+            markTile(tile);
+            listMinesLeft();
+        })
+    })
+})
+boardElement.style.setProperty("--size", BOARD_SIZE);
+minesLeftText.textContent = NUMBER_OF_MINES;
+
+function listMinesLeft() {
+    const markedTilesCount = board.reduce((count, row) => {
+        return count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length;
+    }, 0)
+
+    minesLeftText.textContent = NUMBER_OF_MINES - markedTilesCount;
+}
+
+function checkGameEnd() {
+    const win = checkWin(board);
+    const lose = checkLose(board);
+
+    if (win || lose) {
+        // event occur in capture phase before in bubble phase (tile)
+        boardElement.addEventListener('click', stopProp, { capture: true });
+        boardElement.addEventListener('contextmenu', stopProp, { capture: true });
+    }
+
+    if (win) {
+        messageText.textContent = "You win!";
+    }
+    if (lose) {
+        messageText.textContent = "You lose!"
+        board.forEach(row => {
+            row.forEach
+        })
+    }
+}
+
+function stopProp(e) {
+    // stops the event from going futher down
+    e.stopImmediatePropagation();
+}
