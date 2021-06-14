1053ff46f39db26f95f6d68e14e9ba4bcd89e787
verimascent <verimascent@gmail.com>
2021-06-14 23:30:49 +0800

    Birthday reminder - Setup

---
+++ Birthday Reminder/App.js
@@ -0,0 +1,7 @@
+import React, {useState} from 'react';
+
+function App() {
+    return <h2>reminder project setup</h2>;
+}
+
+export default App;
---
+++ Birthday Reminder/List.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const List = () => {
+    return (
+        <React.Fragment>
+            <h2>list component</h2>
+        </React.Fragment>
+
+    );
+};
+
+export default List;
