72af59843a3b5c4dadc62b70319df7b9f50977df
verimascent <verimascent@gmail.com>
2021-06-16 21:54:33 +0800

    Birthday reminder - npm setup 2

--- Birthday Reminder/src/App.js
+++ Birthday Reminder/src/App.js
@@ -1,7 +1,18 @@
 import React, {useState} from 'react';
+import data from './data';
+import List from './List';
 
 function App() {
-    return <h2>reminder project setup</h2>;
+    const [people, setPeople] = useState(data);
+    return (
+        <main>
+            <section className="container">
+                <h3>0 birthdays today</h3>
+                <List />
+                <button onClick={() => console.log("click me")}>clear all</button>
+            </section>
+        </main>
+    );
 }
 
 export default App;
