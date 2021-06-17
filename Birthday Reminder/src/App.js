b3e534ffe00bb1e85a668704f1703a4e9e037496
verimascent <verimascent@gmail.com>
2021-06-17 21:06:23 +0800

    Birthday reminder - finished and add a new function

--- Birthday Reminder/src/App.js
+++ Birthday Reminder/src/App.js
@@ -7,9 +7,9 @@ function App() {
     return (
         <main>
             <section className="container">
-                <h3>0 birthdays today</h3>
-                <List />
-                <button onClick={() => console.log("click me")}>clear all</button>
+                <h3>{people.length} birthdays today</h3>
+                <List people={people} />
+                <button onClick={() => setPeople([])}>clear all</button>
             </section>
         </main>
     );
--- Birthday Reminder/src/List.js
+++ Birthday Reminder/src/List.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Button } from 'react-bootstrap';
 
-const List = () => {
+const List = ({people}) => {
     return (
         <React.Fragment>
-            <h2>list component</h2>
+            {people.map((person) => {
+                return (
+                    <MyComponent obj={person} key={person.id} />
+                )
+            })
+            }
         </React.Fragment>
 
     );
 };
 
+const MyComponent = (props) => {
+    const [person, setPerson] = useState(props.obj);
+    const {id, name, age, image} = person;
+    return (
+        <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+                <h4>{name}</h4>
+                <p>{age} years</p>   
+            </div>
+            <Button variant="remove" onClick={(e) => e.target.parentNode.parentNode.removeChild(e.target.parentNode)}>Remove</Button>
+        </article>
+    );
+}
 export default List;
