b30b32cc87df198bf3d1da4d29ab1378f0954ac4
verimascent <verimascent@gmail.com>
2021-08-10 11:04:34 +0800

    EasybankLandingPage - webpack config

---
+++ Easybank/webpack.config.js
@@ -0,0 +1,105 @@
+/**
+ * webpack.config.js webpack的配置文件
+ *  作用：指示webpack做什麼事（運行webpack指令時，會加載其中的配置）
+ * 
+ * 所有的構建工具都是基於Node.js平臺運行的
+ * 模塊化默認採用commonjs
+ */
+
+// resolve用來拼接絕對路徑的方法
+const { resolve } = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { resolveCname } = require('dns');
+module.exports = {
+    // webpack 配置
+    entry: './app/js/script.js',
+
+    output: {
+        filename: 'js/built.[hash:10].js',
+        // __dirname Nodejs的變量，代表當前文件的目錄絕對路徑
+        path: resolve(__dirname, 'dist')
+    },
+
+    // loader的配置
+    module: {
+        rules: [
+            // 詳細loader配置
+            {
+                test: /\.css$/,
+                use: [
+                    // use數組中loader執行順序爲從右到左，從上到下，依次執行
+                    // 創建style標籤，將js中的樣式資源插入，添加到head中生效
+                    'style-loader',
+                    // 將css文件變成commonjs模塊加載到js中，裏面內容是樣式字符串
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                // 處理html文件中的img圖片，從而被url-loader處理
+                loader: 'html-loader',
+               
+            },
+            {
+                // 默認無法處理html中img圖片
+                test: /\.(jpe?g|png|gif)$/,
+                // 下載url-loader file-loader
+                loader: 'url-loader',
+                options: {
+                    // 圖片大小小於8kb，就會被base64編碼處理爲字符串
+                    // 優點：減少請求數量（減輕服務器壓力）
+                    // 缺點：圖片體積會更大（文件請求速度更慢）
+                    // 所以只會對小圖片進行base64處理，通常來說8-12kb以下都行
+                    limit: 12 * 1024,
+                    
+                    // 問題：因爲url-loader默認使用es6模塊化解析，而html-loader引入圖片是commonjs，解析時會出問題：[object Module]
+                    // 必須關閉url-loader的es6模塊化，使用commonjs解析
+                    esModule: false,
+
+                    // 給圖片進行重命名
+                    // [hash:10]取圖片的hash的前10位
+                    // [ext]取文件原來的擴展名
+                    name: '[name].[hash:10].[ext]',
+                    outputPath: 'assets/images'
+                
+                }
+            },
+            
+
+            // 其他資源： exclude: , loader: file-loader
+            
+        ]
+    },
+
+    plugins: [
+        // html-webpack-plugin
+        // 功能：默認創建一個空的html，自動引入打包輸出的所有資源（JS/CSS）
+        // 需求：需要有結構的HTML
+        new HTMLWebpackPlugin({
+            template: './index.html',
+            favicon: './assets/images/favicon-32x32.png'
+        }),
+    ],
+
+    mode: 'development',
+
+    // devServer用於自動化
+    // 只會在內存中編譯打包，不會有任何輸出(即不會有dist文件夾產生)
+    // 啓動devServer的指令： npx webpack-dev-server
+    devServer: {
+        contentBase: resolve(__dirname, 'dist'),
+        // 啓動gzip壓縮
+        compress: true,
+        port: 3000,
+        // 自動打開瀏覽器
+        open: true,
+    }
+}
