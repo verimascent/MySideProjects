a2debe8be7161345576a761fec78f5d06d8595c5
verimascent <verimascent@gmail.com>
2021-08-13 23:52:10 +0800

    EasybankLandingPage - third day - hamburger - not finished

--- Easybank/app/js/script.js
+++ Easybank/app/js/script.js
@@ -1,4 +1,12 @@
 import '../scss/style.scss';
 
-// eslint-disable-next-line
-console.log('hellp');
+// eslint-disable-next-line no-undef
+const btnHamburger = document.querySelector('#btnHamburger');
+
+btnHamburger.addEventListener('click', () => {
+  if (btnHamburger.classList.contains('open')) {
+    btnHamburger.classList.remove('open');
+  } else {
+    btnHamburger.classList.add('open');
+  }
+});
