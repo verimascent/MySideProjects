3080f65a08296cf121086656ca2c51d9e6a56c75
verimascent <verimascent@gmail.com>
2021-08-14 23:51:38 +0800

    EasybankLandingPage - fourth day - menu - not finished

--- Easybank/app/js/script.js
+++ Easybank/app/js/script.js
@@ -2,11 +2,17 @@ import '../scss/style.scss';
 
 // eslint-disable-next-line no-undef
 const btnHamburger = document.querySelector('#btnHamburger');
+// eslint-disable-next-line no-undef
+const header = document.querySelector('.header');
+// eslint-disable-next-line no-undef
+const overlay = document.querySelector('.overlay');
 
 btnHamburger.addEventListener('click', () => {
-  if (btnHamburger.classList.contains('open')) {
-    btnHamburger.classList.remove('open');
-  } else {
-    btnHamburger.classList.add('open');
+  if (header.classList.contains('open')) { // close hamburger menu
+    header.classList.remove('open');
+    overlay.classList.remove('fade-in');
+  } else { // open hamburger menu
+    header.classList.add('open');
+    overlay.classList.add('fade-in');
   }
 });
