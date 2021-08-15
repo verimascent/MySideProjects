d648aa2153459534472cdc4b182fa18f6e15853c
verimascent <verimascent@gmail.com>
2021-08-15 23:43:08 +0800

    EasybankLandingPage - fifth day - menu - not finished

--- Easybank/app/js/script.js
+++ Easybank/app/js/script.js
@@ -11,8 +11,10 @@ btnHamburger.addEventListener('click', () => {
   if (header.classList.contains('open')) { // close hamburger menu
     header.classList.remove('open');
     overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
   } else { // open hamburger menu
     header.classList.add('open');
+    overlay.classList.remove('fade-out');
     overlay.classList.add('fade-in');
   }
 });
